a43501cb4cb54a0d9034e83e07c1a01187b162fb
Mand <h17735013379@outlook.com>
2026-06-06 15:20:14 +0800

    chore: 删除误提交的临时测试文件

--- src/bin_test.rs
+++
@@ -1,6 +0,0 @@
-fn main() {
-    let enc = encoding_rs::Encoding::for_label(b"iso-8859-1");
-    println!("for_label iso-8859-1: {:?}", enc.map(|e| e.name()));
-    let enc2 = encoding_rs::Encoding::for_label(b"windows-1252");
-    println!("for_label windows-1252: {:?}", enc2.map(|e| e.name()));
-}
